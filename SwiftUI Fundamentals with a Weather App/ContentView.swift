d345f087b62737b2d55102712f75557d192a53c5
Ivan Trj <ivantrajanovski@gmail.com>
2022-01-05 16:06:39 +0100

    Initial Commit

---
+++ SwiftUI Fundamentals with a Weather App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Fundamentals with a Weather App
+//
+//  Created by Ivan Trajanovski on 2022. 01. 05..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Fundamentals with a Weather App/SwiftUI_Fundamentals_with_a_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Fundamentals_with_a_Weather_AppApp.swift
+//  SwiftUI Fundamentals with a Weather App
+//
+//  Created by Ivan Trajanovski on 2022. 01. 05..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Fundamentals_with_a_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
